16928e2cf69acfbc232734b8d3ac963a233b2d7d
Awais Memon <awaismemon26@gmail.com>
2021-05-27 23:32:00 +0200

    User authentication

---
+++ LoyaltyPrimeDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,24 @@
+﻿using LoyaltyPrimeDataManager.Library.Internal.DataAccess;
+using LoyaltyPrimeDataManager.Library.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoyaltyPrimeDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string Id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var p = new { Id = Id };
+            var output = sql.LoadData<UserModel, dynamic>($"SELECT Id, Username, Email from [dbo].[AspNetUsers] where Id = @Id", p, "DefaultConnection");
+
+            return output;
+        }
+    }
+}
---
+++ LoyaltyPrimeDataManager/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using LoyaltyPrimeDataManager.Library.DataAccess;
+using LoyaltyPrimeDataManager.Library.Models;
+
+using Microsoft.AspNet.Identity;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LoyaltyPrimeDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        [HttpGet]
+        public UserModel GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+            return data.GetUserById(userId).First();
+        }
+    }
+}
